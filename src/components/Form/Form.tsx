ac6ce997b68ed753b37b142c0c54444145f3d34b
Thrywich <aurel.thry@gmail.com>
2023-11-23 11:45:28 +0100

    Init the feature

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,23 @@
+export default function Form({}) {
+    return (
+        <form>
+            <label htmlFor="">
+                <input type="text" />
+            </label>
+
+            <label htmlFor="">
+                <input type="text" />
+            </label>
+
+            <label htmlFor="">
+                <input type="text" />
+            </label>
+            
+            <label htmlFor="">
+                <input type="text" />
+            </label>
+
+            <button type="submit"></button>
+        </form>
+    )
+}
